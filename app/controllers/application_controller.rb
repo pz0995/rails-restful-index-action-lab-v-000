f4a147673a4730aadc9d6e0c069cb9a6c92e8b95
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-10-15 13:44:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+
+
+class StudentController < ApplicationController
+  def index
+    @students = Student.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+   # map.resources :students
+   # root :to => "students#index"
+  # get 'views/students/index.html.erb', to: 'students#index'
+  #  resources :students, only: [:index, :show]
+  get :students => "students#show"
+  resources :students
+#  , only: :index, :show
 end
