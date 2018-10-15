eba69d6338f7ab4b953768e04a293119ebd31448
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-10-15 13:45:20 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-   # map.resources :students
-   # root :to => "students#index"
-  # get 'views/students/index.html.erb', to: 'students#index'
-  #  resources :students, only: [:index, :show]
   get :students => "students#show"
   resources :students
-#  , only: :index, :show
+
 end
